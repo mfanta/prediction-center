8ca43bd6de97a9291199a91b377292be9dcb77ed
Martin Fanta <mafanta@cisco.com>
2016-02-12 18:12:48 +0100

    Remove unnecessary modifiers.

--- src/main/java/cz/mfanta/tip_centrum/service/result/IResultService.java
+++ src/main/java/cz/mfanta/tip_centrum/service/result/IResultService.java
@@ -7,13 +7,13 @@ public interface IResultService extends IService {
 	/**
 	 * Updates the database with latest results.
 	 */
-	public void updateAllResults();
+	void updateAllResults();
 
 	/**
 	 * Updates the database with latest results for a particular competition.
 	 *
 	 * @param competitionName The competition whose results should be updated.
 	 */
-	public void updateResultsForCompetition(String competitionName);
+	void updateResultsForCompetition(String competitionName);
 
 }
