92a98880e8b77a8969f46c3065ecd4fc513bbf14
Martin Fanta <mafanta@cisco.com>
2016-05-13 17:26:39 +0200

    Creating a wrapper class for Fixture Table.

---
+++ src/main/java/cz/mfanta/tip_centrum/service/gui/FixtureTableWrapper.java
@@ -0,0 +1,16 @@
+package cz.mfanta.tip_centrum.service.gui;
+
+import javax.swing.*;
+
+public class FixtureTableWrapper {
+
+    private JTable fixtureTable;
+
+    public void create() {
+        // TODO: implement FixtureTableWrapper.create()
+    }
+
+    public void scrollToEnd() {
+        // TODO: implement FixtureTableWrapper.scrollToEnd()
+    }
+}
