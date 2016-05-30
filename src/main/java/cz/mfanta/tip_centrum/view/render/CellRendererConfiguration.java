7ce650046b35080968f1efc081aa5930273e86ee
Martin Fanta <mafanta@cisco.com>
2016-05-30 17:16:07 +0200

    Adding TeamCellRenderer to Spring configuration.

--- src/main/java/cz/mfanta/tip_centrum/view/render/CellRendererConfiguration.java
+++ src/main/java/cz/mfanta/tip_centrum/view/render/CellRendererConfiguration.java
@@ -28,4 +28,9 @@ public class CellRendererConfiguration {
                 colorPicker()
         );
     }
+
+    @Bean
+    public TeamCellRenderer teamCellRenderer() {
+        return new TeamCellRenderer();
+    }
 }
