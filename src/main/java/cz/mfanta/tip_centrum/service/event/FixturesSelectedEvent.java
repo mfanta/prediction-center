ae5d4e5f7f2ccedaa73f0525276053b1476392c3
Martin Fanta <mafanta@cisco.com>
2016-05-30 17:49:52 +0200

    Adding @EqualsAndHashCode to FixturesSelectedEvent.

--- src/main/java/cz/mfanta/tip_centrum/service/event/FixturesSelectedEvent.java
+++ src/main/java/cz/mfanta/tip_centrum/service/event/FixturesSelectedEvent.java
@@ -2,10 +2,12 @@ package cz.mfanta.tip_centrum.service.event;
 
 import cz.mfanta.tip_centrum.entity.IFixtureGroup;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Builder
 @Getter
+@EqualsAndHashCode
 public class FixturesSelectedEvent {
 
     private final IFixtureGroup selectedFixtures;
