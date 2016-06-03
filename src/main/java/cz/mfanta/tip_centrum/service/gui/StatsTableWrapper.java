ec67552d32258a22e64bac39f4ff1565d2df37b4
Martin Fanta <mafanta@cisco.com>
2016-06-03 15:52:12 +0200

    Handling stats model refresh.

--- src/main/java/cz/mfanta/tip_centrum/service/gui/StatsTableWrapper.java
+++ src/main/java/cz/mfanta/tip_centrum/service/gui/StatsTableWrapper.java
@@ -1,11 +1,14 @@
 package cz.mfanta.tip_centrum.service.gui;
 
+import com.google.common.eventbus.Subscribe;
+import cz.mfanta.tip_centrum.service.event.StatsModelRefreshedEvent;
 import cz.mfanta.tip_centrum.view.model.StatsTableDesign;
 import cz.mfanta.tip_centrum.view.model.StatsTableModel;
 import lombok.Builder;
 import lombok.Getter;
 
 import javax.swing.*;
+import javax.swing.event.TableModelEvent;
 import javax.swing.table.TableColumnModel;
 
 @Builder
@@ -15,6 +18,15 @@ class StatsTableWrapper {
 
     @Getter
     private JTable statsTable;
+
+    @Subscribe
+    public void handleModelRefresh(StatsModelRefreshedEvent event) {
+        statsTable.tableChanged(new TableModelEvent(
+                statsTableModel,
+                0,
+                statsTableModel.getRowCount() - 1
+        ));
+    }
 
     void create() {
         statsTable = new JTable(statsTableModel);
