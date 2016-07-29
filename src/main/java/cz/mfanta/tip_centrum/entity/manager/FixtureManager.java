f43127eb7f1d0cc25e1ea68169c868d0c7bb855c
Martin Fanta <mafanta@cisco.com>
2016-07-29 17:37:18 -0400

    Removing unnecessary 'public' class modifier.

--- src/main/java/cz/mfanta/tip_centrum/entity/manager/FixtureManager.java
+++ src/main/java/cz/mfanta/tip_centrum/entity/manager/FixtureManager.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Set;
 
 @RequiredArgsConstructor
-public class FixtureManager implements IFixtureManager {
+class FixtureManager implements IFixtureManager {
 
 	private final IFixtureReader fixtureReader;
 	
